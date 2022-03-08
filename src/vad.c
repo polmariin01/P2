7e3fb250ea319401712fda5956d5430258aa8e2d
gilboleda <gilboleda@gmail.com>
2022-03-08 14:03:27 +0100

    codi modificat a l'aula

--- src/vad.c
+++ src/vad.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "pav_analysis.h"
 #include "vad.h"
 
 const float FRAME_TIME = 10.0F; /* in ms. */
@@ -42,7 +43,10 @@ Features compute_features(const float *x, int N) {
    * For the moment, compute random value between 0 and 1 
    */
   Features feat;
-  feat.zcr = feat.p = feat.am = (float) rand()/RAND_MAX;
+  //feat.zcr = feat.p = feat.am = (float) rand()/RAND_MAX;
+  feat.zcr = compute_zcr(x,N,16000);
+  feat.am = compute_am(x,N);
+  feat.p = compute_power(x,N);
   return feat;
 }
 
@@ -93,12 +97,13 @@ VAD_STATE vad(VAD_DATA *vad_data, float *x) {
     break;
 
   case ST_SILENCE:
-    if (f.p > 0.95)
+    if (f.p > vad_data->p1)
       vad_data->state = ST_VOICE;
+      vad_data->p1 = f.p + 10;
     break;
 
   case ST_VOICE:
-    if (f.p < 0.01)
+    if (f.p < vad_data->p1)
       vad_data->state = ST_SILENCE;
     break;
 
